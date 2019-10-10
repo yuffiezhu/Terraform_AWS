0382fcc13bb8c914fbfa2ec10a28202aa562dde1
yuffie <zhuy@US-ZHUY.fortinet-us.com>
2019-10-09 17:18:38 -0700

    completed chapter2

---
+++ chapter2/singleServer/main.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+variable "server_port" {
+    description = "The port the server will use for HTTP requests"
+    default = "8080"
+}
+
+resource "aws_instance" "example" {
+    ami           = "ami-41e7cb24"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = ["${aws_security_group.instance.id}"]   
+    //"${TYPE.NAME.ATTRIBUTE}"
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World!" > index.html
+                nohup busybox httpd -f -p "${var.server_port}" &
+                EOF
+
+    tags = {
+        Name = "terraform-example"
+    }
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance1"
+
+    ingress {
+        from_port = "${var.server_port}"
+        to_port   = "${var.server_port}"
+        protocol  = "tcp"
+        cidr_blocks = ["0.0.0.0/0"] 
+    }
+}
+
+output "public_ip" {
+    value = "${aws_instance.example.public_ip}"
+}
